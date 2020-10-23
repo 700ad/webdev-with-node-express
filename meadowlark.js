11eeeed7d9298dd078a54642017506f7a0b460d2
Adeel Farzand <adeel.farzand.1@gmail.com>
2020-10-23 12:29:17 +0500

    Added Unit Tests For Routes.

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const expresshandlebars = require('express-handlebars');
 const fortune = require('./lib/fortune');
+const handlers = require('./lib/handlers');
 const app = express();
 
 app.engine('handlebars', expresshandlebars({
@@ -12,26 +13,12 @@ app.use(express.static(__dirname + '/public'));
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.type('text/plain')
-    res.send('Meadowlark Travel')
-})
+app.get('/', handlers.home)
 
-app.get('/about', (req, res) => {
-    res.render('about', { fortune: fortune.getFortune() })
-})
+app.get('/about', handlers.about)
 
-app.use((req, res) => {
-    res.status(404)
-    res.render('404')
-})
+app.use(handlers.notFound)
 
-app.use((req, res) => {
-    console.error(err.message);
-    res.status(500)
-    res.render('500')
-})
-
-
+app.use(handlers.serverError)
 
 app.listen(port, () => console.log(`Express Started on Localhost ${port}`));
