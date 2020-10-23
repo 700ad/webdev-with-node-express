fe09f9487469990a264cc9ad1e58de21d2409811
Adeel Farzand <adeel.farzand.1@gmail.com>
2020-10-23 09:22:25 +0500

    Created 404 and 500 Routes.

---
+++ meadowlark.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use((req, res) => {
+    res.type('text/plain')
+    res.status(404)
+    res.send('404 - Not Found')
+})
+
+app.use((req, res) => {
+    console.error(err.message);
+    res.type('text/plain')
+    res.status(500)
+    res.send('500 - Server Error')
+})
+
+app.listen(port, () => console.log(`Express Started on Localhost ${port}`));
